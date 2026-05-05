c9d38712e4132162b2d7dfd30f0fe1b22533abdb
Jaycup-J <jakubjdrugiekonto533@gmail.com>
2026-05-05 13:37:42 +0200

    Add files via upload

---
+++ zadanie1/plik.py
@@ -0,0 +1,10 @@
+for i in range(1, 101):
+    string = ""
+    if (i % 3 == 0):
+        string += "Fizz"
+    if (i % 5 == 0):
+        string += "Buzz"
+    if (string != ""):
+        print(string)
+    else:
+        print(i)
